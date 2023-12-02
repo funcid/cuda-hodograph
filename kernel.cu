0f9b69e64bb83820718caec2025dd9f16bf53160
Артем Царюк <rtem903134@gmail.com>
2023-12-02 14:59:53 +0300

    launch function solving

--- kernel.cu
+++ kernel.cu
@@ -1,42 +1,36 @@
 #include <stdio.h>
+#define N (1024*1024)
 
-__global__
-void saxpy(int n, float a, float *x, float *y)
-{
-  int i = blockIdx.x*blockDim.x + threadIdx.x;
-  if (i < n) y[i] = a*x[i] + y[i];
+__global__ void kernel(float* dA) { 
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  float x = 2.0f * 3.1415926f * (float) idx / (float) N;
+  dA [idx] = sinf(sqrtf(x));
 }
 
-int main(void)
-{
-  int N = 1<<20;
-  float *x, *y, *d_x, *d_y;
-  x = (float*)malloc(N*sizeof(float));
-  y = (float*)malloc(N*sizeof(float));
+bool checkLaunched() {
+  cudaError_t err;
+  cudaDeviceSynchronize();
+  err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    fprintf(stderr, "Cannot launch CUDA kernel: %s\n", cudaGetErrorString(err));
+    return false;
+  } 
+  return true;
+}
 
-  cudaMalloc(&d_x, N*sizeof(float)); 
-  cudaMalloc(&d_y, N*sizeof(float));
+int main(void) {
+  float *hA, *dA; int cudaCores = 1024;
+  hA = (float*) malloc (N * sizeof(float));
+  cudaMalloc((void**) &dA, N * sizeof(float));
+  kernel <<< N / cudaCores, cudaCores >>> (dA);
+  cudaMemcpy(hA, dA, N * sizeof(float), cudaMemcpyDeviceToHost);
 
-  for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
+  if (!checkLaunched()) return 1;
+
+  for (int idx = 0; idx < N; idx++) {
+    printf("a[%d] = %.5f\n", idx, hA[idx]);
   }
-
-  cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
-
-  // Perform SAXPY on 1M elements
-  saxpy<<<(N+255)/256, 256>>>(N, 2.0f, d_x, d_y);
-
-  cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
-
-  float maxError = 0.0f;
-  for (int i = 0; i < N; i++)
-    maxError = max(maxError, abs(y[i]-4.0f));
-  printf("Max error: %f\n", maxError);
-
-  cudaFree(d_x);
-  cudaFree(d_y);
-  free(x);
-  free(y);
+  free(hA); cudaFree(dA);
+  
+  return 0;
 }
