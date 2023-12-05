c98b456771f95a9fde9cff0de60c5397e04fb378
Артем Царюк <rtem903134@gmail.com>
2023-12-05 20:18:53 +0300

    draw graphics

--- src/main.cu
+++ src/main.cu
@@ -2,107 +2,112 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#define N (4096*4096)
+#define WIDTH (1000)
+#define HEIGHT (800)
+#define N (2048*2048)
 #define CORES (1024)
 
-__global__ void kernel(float* dA) { 
+__global__ void kernel(float* dA) 
+{ 
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  float x = 2.0f * 3.1415926f * (float) idx / (float) N;
-  dA[idx] = sinf(sqrtf(x));
+  dA[idx] = 1.0 / (100 * (float) (idx - N / 2) / (float) N);
 }
 
-bool checkLaunched() {
-  cudaError_t err;
-  cudaDeviceSynchronize();
-  err = cudaGetLastError();
-  if (err != cudaSuccess) {
-    fprintf(stderr, "Cannot launch CUDA kernel: %s\n", cudaGetErrorString(err));
-    return false;
-  } 
-  return true;
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void checkCudaLaunched() 
 {
-    //glViewport(0, 0, width, height);
+  cudaDeviceSynchronize();
+  cudaError_t err = cudaGetLastError();
+  if (err != cudaSuccess) 
+  {
+    fprintf(stderr, "Cannot launch CUDA kernel: %s\n", cudaGetErrorString(err));
+    exit(err);
+  } 
 }
 
-int main(void) {
+void checkGlfwAndGlewInit() 
+{
   if (!glfwInit()) {
     printf("Failed to initialize GLFW");
-      return -1;
+    exit(-389);
   }
   if (!glewInit()) {
     printf("Failed to initialize GLEW");
-      return -1;
+    exit(-340);
   }
+}
 
-  GLFWwindow* window;
-  window = glfwCreateWindow(800, 600, "Hodograph", NULL, NULL);
-  if (window == NULL)
-  {
-    printf("Failed to open GLFW window");
-      return -1;
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void renderFrame(float* result) 
+{
+  glBegin(GL_LINE_STRIP);
+  glVertex2f(-1, 0);
+  glVertex2f(1, 0);
+  glEnd();
+  glBegin(GL_LINE_STRIP);
+  glVertex2f(0, 1);
+  glVertex2f(0, -1);
+  glEnd();
+  glBegin(GL_LINE_STRIP);
+  for (int i = 0; i < N; i++) { 
+    glVertex2f(2 * (i * 1.0 / N - 0.5), result[i]);
   }
-  glfwMakeContextCurrent(window);
+  glEnd();
+}
 
-  printf("\nRenderer: %s", glGetString(GL_RENDERER));
-  printf("\nOpenGL version: %s", glGetString(GL_VERSION));
-
-  glViewport(0, 0, 800, 600);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-  while(!glfwWindowShouldClose(window)) {
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glfwPollEvents();    
-    glBegin(GL_TRIANGLES);
-    glVertex2f(-0.5, -0.5);
-    glVertex2f(0.0, 0.5);
-    glVertex2f(0.5, -0.5);
-    glEnd();
-    glfwSwapBuffers(window);
-  }
-
-  glfwTerminate();
-
+int main(void) 
+{
+  // CUDA calculations
   float timerValueGPU, timerValueCPU;
   cudaEvent_t start, stop;
   cudaEventCreate(&start);
   cudaEventCreate(&stop);
   cudaEventRecord(start, 0);
   
-  float *hA, *dA;
-  hA = (float*) malloc(N * sizeof(float));
-  cudaMalloc((void**) &dA, N * sizeof(float));
-  kernel <<< N / CORES, CORES >>> (dA);
-  cudaMemcpy(hA, dA, N * sizeof(float), cudaMemcpyDeviceToHost);
-
-  if (!checkLaunched()) return 1;
+  float *hostArray, *deviceArray;
+  hostArray = (float*) malloc(N * sizeof(float));
+  cudaMalloc((void**) &deviceArray, N * sizeof(float));
+  kernel<<< N / CORES, CORES >>>(deviceArray);
+  cudaMemcpy(hostArray, deviceArray, N * sizeof(float), cudaMemcpyDeviceToHost);
+  checkCudaLaunched();
+  checkGlfwAndGlewInit();
 
   cudaEventRecord(stop, 0);
   cudaEventSynchronize(stop);
   cudaEventElapsedTime(&timerValueGPU, start, stop);
-  printf("\n GPU calculation time: %f ms\n", timerValueGPU);
+  printf("GPU calculation time: %f ms\n", timerValueGPU);
 
-  //for (int idx = 0; idx < N; idx++) {
-  //  if (idx % 10000 == 0) {
-  //    printf("a[%d] = %.5f\n", idx, hA[idx]);
-  //  }
-  //}
-
-  cudaEventRecord(start, 0);
-  for (int i = 0; i < N; i++) {
-    hA[i] = sinf(sqrtf(2.0f * 3.1415926f * (float) i / (float) N));
-  }
-  cudaEventRecord(stop, 0);
-  cudaEventSynchronize(stop);
-  cudaEventElapsedTime(&timerValueCPU, start, stop);
-  printf("\n CPU calculation time: %f ms\n", timerValueCPU);
-  printf("\n Rate: %fx\n", timerValueCPU / timerValueGPU);
-
-  free(hA); cudaFree(dA);
+  cudaFree(deviceArray);
   cudaEventDestroy(start);
   cudaEventDestroy(stop);
+
+  // Windows application open
+  GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Hodograph", NULL, NULL);
+  if (window == NULL)
+  {
+    printf("Failed to open GLFW window");
+    exit(-341);
+  }
+
+  glfwMakeContextCurrent(window);
+  glViewport(0, 0, WIDTH, HEIGHT);
+  glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+  printf("Renderer: %s\n", glGetString(GL_RENDERER));
+  printf("OpenGL version: %s\n", glGetString(GL_VERSION));
+
+  while(!glfwWindowShouldClose(window)) 
+  {
+    glfwPollEvents();  
+    glClear(GL_COLOR_BUFFER_BIT);
+    renderFrame(hostArray);
+    glfwSwapBuffers(window);
+  }
+
+  free(hostArray); 
+  glfwTerminate();
   return 0;
 }
