096516191409b42a9a3b0a29f2c507e7de0191d7
reidjJava <reidjJava@gmail.com>
2023-12-15 13:34:58 +0300

    try

--- src/render/render.cu
+++ src/render/render.cu
@@ -44,7 +44,7 @@ void renderFrame(float* result)
 
   glBufferData(GL_ARRAY_BUFFER, N * sizeof(float), result, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 1, GL_FLOAT, GL_FALSE, sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 1, GL_FLOAT, GL_FALSE, 0, (void*)0);
   glEnableVertexAttribArray(0);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
